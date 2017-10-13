75e0ead1c7ae0835626642eb55f80b669c04fdf5
John Hu <im@john.hu>
2017-10-13 13:34:07 +0800

    read rss news from hard-coded site

--- App.js
+++ App.js
@@ -1,37 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
+  FlatList,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import { parseString } from 'react-native-xml2js';
+import _ from 'lodash';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const FEED_LIST = ['https://feeds.feedburner.com/TheNewsLens'];
 
 export default class App extends Component<{}> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+    this.renderListItem = this.renderListItem.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchFeeds();
+  }
+
+  convertToFeedItem(url, rssItem) {
+    const link = rssItem.link[0] || _.get(rssItem, 'feedburner:origLink[0]');
+    return {
+      key: `${url}$${link}`,
+      sourceURL: url,
+      categories: rssItem.category,
+      title: rssItem.title.join(', '),
+      description: rssItem.description.join(', '),
+      pubDate: rssItem.pubDate[0],
+      link: link
+    };
+  }
+
+  addXMLFeed(url, xml) {
+    const newItems = [...this.state.items];
+    const channels = _.get(xml, 'rss.channel');
+    _.forEach(channels, (channel) => {
+      _.forEach(channel.item, (item) => {
+        newItems.push(this.convertToFeedItem(url, item));
+      });
+    });
+
+    this.setState({ items: newItems });
+  }
+
+  fetchFeeds() {
+    FEED_LIST.forEach((url) => {
+      fetch(url).then((response) => {
+        return response.text();
+      }).then((text) => {
+        return new Promise((resolve, reject) => {
+          parseString(text, (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          });
+        });
+      }).then((xml) => {
+        this.addXMLFeed(url, xml);
+      });
+    });
+  }
+
+  renderListItem({item, index}) {
+    return (<Text style={styles.listItem}>{item.title}</Text>);
+  }
+
   render() {
+    const { items } = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Items Count: {items.length}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <FlatList
+          data={items}
+          renderItem={this.renderListItem}
+          style={styles.list} />
       </View>
     );
   }
@@ -41,17 +93,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    flexGrow: 0,
+    flexShrink: 0
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  list: {
+    flexGrow: 1,
+    flexShrink: 1
   },
+
+  listItem: {
+    borderBottomColor: '#888888',
+    borderBottomWidth: 2,
+    fontSize: 20
+  }
 });
